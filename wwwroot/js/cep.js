ed89f53b015f210e820d8b3934bd879904cd650e
remix-pc <81277029+remix-pc@users.noreply.github.com>
2022-11-08 14:43:15 -0300

    Add files via upload

---
+++ wwwroot/js/cep.js
@@ -0,0 +1,45 @@
+/*REGEX */
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+const cepValido = (cep)=> cep.length == 8 && eNumero(cep);
+
+const pesquisarCEP = async() =>{
+    limparFormulario();
+
+    const cep = document.getElementById('input_cep').value.replace("-","");
+    const url = `https://viacep.com.br/ws/${cep}/json`;
+
+    if(cepValido(cep)){
+        const dados = await fetch(url);
+        const endereco = await dados.json();
+        if(endereco.hasOwnProperty('erro')){
+            document.getElementById('endereco').value = 'CEP não encontrado'
+
+        }else{
+            preencherFormulario(endereco);
+        }
+    }else{
+        document.getElementById('endereco').value = 'CEP incorreto'
+    }
+}
+
+document.getElementById('input_cep').addEventListener('focusout',pesquisarCEP)
+
+
+const limparFormulario = (endereco) =>{
+    document.getElementById('endereco').value = "";
+    /*document.getElementById('bairro').value = "";
+    document.getElementById('cidade').value = "";
+    document.getElementById('estado').value = "";*/
+}
+
+const preencherFormulario = (endereco) =>{
+    document.getElementById('endereco').value = endereco.logradouro;
+   /* document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.cidade;
+    document.getElementById('estado').value = endereco.uf;*/
+}
+
+const endereco2 = endereco
+
+document.getElementById('endereco2').value = endereco2
